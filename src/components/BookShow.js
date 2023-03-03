39d6cd87080b5b26c8f4772df6308d9a267458a2
phaiza <phaiza@gmail.com>
2023-03-03 11:46:08 +1100

    asecond

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,13 +1,29 @@
 import { useState } from 'react';
+import BookEdit from './BookEdit';
 import './BookShow.css';
 const BookShow = ({ book, deleteBookFunc }) => {
+  const [showEdit, setShowEdit] = useState(false);
+  if (showEdit) {
+    content = <BookEdit />;
+  }
   const handleDelete = () => {
     deleteBookFunc(book.id);
   };
+  let content = <h2>{book.title}</h2>;
+  const handleEdit = () => {
+    setShowEdit(!showEdit);
+  };
   return (
-    <div className="book-card">
-      <h2>{book.title}</h2>
-      <button onClick={handleDelete}>Delete</button>
+    <div className="book-show">
+      {content}
+      <div className="actions">
+        <button className="delete" onClick={handleDelete}>
+          Delete
+        </button>
+        <button className="edit" onClick={handleEdit}>
+          Edit
+        </button>
+      </div>
     </div>
   );
 };
